eda2e61088829264d7828f9c2435886161c24a95
ionnuthub <raducugionut@gmail.com>
2023-10-27 18:04:05 +0200

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `98ed0352-cfe7-4ca6-8942-4dbaec8017e4`;
+export const MODAL_CLOSE_SEC = 2.5;
